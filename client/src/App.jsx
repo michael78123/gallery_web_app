38e89b2e64fa71ea52c86580e11fcf161d3df959
Michael Liou <michael78123@gmail.com>
2024-03-22 16:01:43 -0700

    add vote count, vote button UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Image from "./Image";
+import "./App.css";
 
 const App = () => {
   const [images, setImages] = useState([]);
--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import "./image.css";
 
-const Image = ({id, url, upvoteCount, downvoteCount}) => {
-  return <div><img src={url} /></div>;
+const Image = ({ id, url, upvoteCount, downvoteCount }) => {
+  return (
+    <div className="card">
+      <img src={url} width="300" height="300" />
+      <div className="info">
+        <div className="vote_box">
+          <div className="vote_count">{`upvote count: ${upvoteCount}`}</div>
+          <div className="vote_button"> UPVOTE </div>
+        </div>
+        <div className="vote_box">
+          <div className="vote_count">{`downvote count: ${downvoteCount}`}</div>
+          <div className="vote_button"> DONWVOTE </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Image;
